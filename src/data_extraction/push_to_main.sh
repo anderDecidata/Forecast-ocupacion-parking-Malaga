2c17b62ee56bca8dcf86af02c6254e5583e029ac
Ander Fernandez <afernandez@decidata.es>
2022-04-11 10:35:39 +0200

    Bug fix in commit

--- src/data_extraction/push_to_main.sh
+++ src/data_extraction/push_to_main.sh
@@ -3,7 +3,7 @@ if [[ "$(git status --porcelain)" != "" ]]; then
     git config --global user.name 'Ander Fernández'
     git config --global user.email 'afernandez@decidata.es'
     git add data/raw.csv
-    git commit -am "Automatically updated raw data"
+    git commit -m "Automatically updated raw data"
     git push origin main
 else
 echo "Nothing to commit..."
