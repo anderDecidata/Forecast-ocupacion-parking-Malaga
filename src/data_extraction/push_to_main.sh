91ac0b74f5f01988768d74221465307b6f5ad2aa
Ander Fernandez <afernandez@decidata.es>
2022-04-11 11:00:22 +0200

    Update git commit -m to -am

--- src/data_extraction/push_to_main.sh
+++ src/data_extraction/push_to_main.sh
@@ -2,5 +2,5 @@
 git config --global user.name 'Ander Fernández'
 git config --global user.email 'afernandez@decidata.es'
 git add data/raw.csv
-git commit -m "Automatically updated raw data"
+git commit -am "Automatically updated raw data"
 git push origin main
